18dec9d5f90afcb6ee84296676251479e0baa77e
depthbomb <reesepup77@hotmail.com>
2020-06-16 00:54:58 -0500

    Bump version

--- CaprineNetChecker/AppVersion.cs
+++ CaprineNetChecker/AppVersion.cs
@@ -37,7 +37,7 @@ namespace CaprineNetChecker
         public static Version AsDotNetVersion() => new Version(Major, Minor, Patch, Hotfix);
         public static int Major => 1;
         public static int Minor => 0;
-        public static int Patch => 0;
+        public static int Patch => 1;
         public static int Hotfix => 0;
         public static ReleaseTypes ReleaseType => ReleaseTypes.Release;
         public static string SemVer => $"{Major}.{Minor}.{Patch}.{Hotfix}";
